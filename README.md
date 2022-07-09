cb30bc1b54e8e8dd3129658feba2e15b1d0f5219
ivy <aibiimaknae@gmail.com>
2022-07-09 04:04:39 -0500

    finished css html javascript. updated readme with links to resources i used that helped me out

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # First-Milestone-Project-2-Electric-Boogaloo
 
-a very pinky game of tic tac toe. maybe also cuteified.
+a tic-tac-toe game inspired by all shades of pink. pretty self-explanatory. uses the objectively cute, rounded font of ubuntu and includes the occasional heart and sparkle. the only main bug is the new game button moving when the winner message pops up. it usually resets when the winner message disappears.
+
+references for this game : https://www.webtips.dev/tic-tac-toe-in-javascript 
+            https://www.codebrainer.com/blog/tic-tac-toe-javascript-game
+            https://dev.to/bornasepic/pure-and-simple-tic-tac-toe-with-javascript-4pgn
+            https://www.youtube.com/watch?v=B3pmT7Cpi24&t=409s 
+
+color palettes used : https://www.colourlovers.com/palette/1465585/Dont_Call_Me_Baby
+ https://www.colourlovers.com/palette/1684068/SUNRISE_IN_TOKYO 
---
+++ script.js
@@ -0,0 +1,129 @@
+
+window.addEventListener('DOMContentLoaded', () => {
+    const tiles = Array.from(document.querySelectorAll('.cell'));
+    const playerDisplay = document.querySelector('.display-player');
+    const resetButton = document.querySelector('.new-game');
+    const announcer = document.querySelector('.announcer');
+
+    // multiple sources had the board set up as an empty array of strings to count for each cell we made in the html.
+    let board = ['', '', '', '', '', '', '', '', ''];
+    let currentPlayer = 'X';
+    let isGameActive = true;
+
+    const PLAYERX_WON = 'PLAYERX_WON';
+    const PLAYERO_WON = 'PLAYERO_WON';
+    const TIE = 'TIE';
+
+
+    /*
+        Indexes within the board
+        [0] [1] [2]
+        [3] [4] [5]
+        [6] [7] [8]
+    */
+
+    // this seems to be the most common way to set up the winning conditions, multiple different sources i saw before i even started coding this game used this method
+    // it is an array of arrays that lists out every possible win condition with a combination of rows, columns, and diagonals with the cells starting at 0 and ending at 8
+    const winningConditions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    // runs after each turn to see if someone has won or if there is a tie. the continue tells it to keep going while the break signifies that the game has been won
+    function handleResultValidation() {
+        let roundWon = false;
+        for (let i = 0; i <= 7; i++) {
+            const winCondition = winningConditions[i];
+            const a = board[winCondition[0]];
+            const b = board[winCondition[1]];
+            const c = board[winCondition[2]];
+            if (a === '' || b === '' || c === '') {
+                continue;
+            }
+            if (a === b && b === c) {
+                roundWon = true;
+                break;
+            }
+        }
+
+    if (roundWon) {
+            announce(currentPlayer === 'X' ? PLAYERX_WON : PLAYERO_WON);
+            isGameActive = false;
+            return;
+        }
+
+    if (!board.includes(''))
+        announce(TIE);
+    }
+
+    const announce = (type) => {
+        switch(type){
+            case PLAYERO_WON:
+                announcer.innerHTML = '✨Player <span class="playerO">O</span> Won✨';
+                break;
+            case PLAYERX_WON:
+                announcer.innerHTML = '✨Player <span class="playerX">X</span> Won✨';
+                break;
+            case TIE:
+                announcer.innerText = 'Tie';
+        }
+        announcer.classList.remove('hide');
+    };
+
+    const isValidAction = (tile) => {
+        if (tile.innerText === 'X' || tile.innerText === 'O'){
+            return false;
+        }
+
+        return true;
+    };
+
+    const updateBoard =  (index) => {
+        board[index] = currentPlayer;
+    }
+
+    const changePlayer = () => {
+        playerDisplay.classList.remove(`player${currentPlayer}`);
+        currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+        playerDisplay.innerText = currentPlayer;
+        playerDisplay.classList.add(`player${currentPlayer}`);
+    }
+
+    const userAction = (tile, index) => {
+        if(isValidAction(tile) && isGameActive) {
+            tile.innerText = currentPlayer;
+            tile.classList.add(`player${currentPlayer}`);
+            updateBoard(index);
+            handleResultValidation();
+            changePlayer();
+        }
+    }
+    
+    const resetBoard = () => {
+        board = ['', '', '', '', '', '', '', '', ''];
+        isGameActive = true;
+        announcer.classList.add('hide');
+
+        if (currentPlayer === 'O') {
+            changePlayer();
+        }
+
+        tiles.forEach(tile => {
+            tile.innerText = '';
+            tile.classList.remove('playerX');
+            tile.classList.remove('playerO');
+        });
+    }
+
+    tiles.forEach( (tile, index) => {
+        tile.addEventListener('click', () => userAction(tile, index));
+    });
+
+    resetButton.addEventListener('click', resetBoard);
+});
